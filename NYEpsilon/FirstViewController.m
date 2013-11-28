a2d897e8247a0f6829b0c20e15fe5ea0e7b3fecb
Stephen Silber <stephen@Stephens-MacBook-Pro.local>
2013-11-27 21:07:24 -0500

    Initial Commit

---
+++ NYEpsilon/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  NYEpsilon
+//
+//  Created by Stephen Silber on 11/27/13.
+//  Copyright (c) 2013 Stephen Silber. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ NYEpsilonTests/NYEpsilonTests.m
@@ -0,0 +1,34 @@
+//
+//  NYEpsilonTests.m
+//  NYEpsilonTests
+//
+//  Created by Stephen Silber on 11/27/13.
+//  Copyright (c) 2013 Stephen Silber. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NYEpsilonTests : XCTestCase
+
+@end
+
+@implementation NYEpsilonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
