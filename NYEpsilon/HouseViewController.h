5a0440d3c011637501f5d2909b6890b2f70cf989
Stephen Silber <stephen@Stephens-MacBook-Pro.local>
2013-11-27 22:12:32 -0500

    Initialized HouseViewController class

---
+++ NYEpsilon/HouseViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  NYEpsilon
+//
+//  Created by Stephen Silber on 11/27/13.
+//  Copyright (c) 2013 Stephen Silber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HouseViewController : UIViewController
+
+@end
